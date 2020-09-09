0547c3817c40968678043c271797f981aec7757b
EMCain <>
2020-09-08 21:33:47 -0700

    add date field, make main page list candidates + races

--- app/models.py
+++ app/models.py
@@ -1,5 +1,27 @@
+from datetime import date
+
 from app import db 
 from sqlalchemy.schema import PrimaryKeyConstraint
+
+
+class CandidateRace(db.Model):
+    """
+    the candidate is running in the race (or considering it, has run, etc.)
+    """
+    candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.id'), nullable=False)
+    race_id = db.Column(db.Integer, db.ForeignKey('race.id'), nullable=False)
+    status = db.Column(db.Integer) # TODO add a constraint; this can be 'declared', 'considering', 'won', 'lost', etc. 
+    write_in = db.Column(db.Boolean, default=False) 
+
+    candidate = db.relationship('Candidate', foreign_keys='CandidateRace.candidate_id')
+    race = db.relationship('Race', foreign_keys='CandidateRace.race_id')
+
+    __table_args__ = (
+        PrimaryKeyConstraint(candidate_id, race_id),
+    )
+
+cand_race = CandidateRace
+
 
 class Candidate(db.Model):
     """
@@ -13,29 +35,22 @@ class Candidate(db.Model):
     zip_code_1 = db.Column(db.String(5))
     zip_code_2 = db.Column(db.String(4))
 
-    # TODO many to many with Race 
-
-class CandidateRace(db.Model):
-    """
-    the candidate is running in the race (or considering it, has run, etc.)
-    """
-    candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.id'), nullable=False)
-    race_id = db.Column(db.Integer, db.ForeignKey('race.id'), nullable=False)
-    status = db.Column(db.Integer) # TODO add a constraint; this can be 'declared', 'considering', 'won', 'lost', etc. 
-    write_in = db.Column(db.Boolean, default=False) 
-
-    __table_args__ = (
-        PrimaryKeyConstraint(candidate_id, race_id),
-    )
+    # TODO many to many with Race
+    # races = db.relationship('Race', secondary='candidate_race', lazy='subquery',
+    #    backref=db.backref('candidates', lazy=True))
 
 class Race(db.Model):
     # in the sense of "election contest for a specific seat," not "ethnic group"
     # Not sure if we want to have an "election" object or not but it's low priority
     id = db.Column(db.Integer, primary_key=True)
-    # date = db.Column(db.Date(), null=False)  getting weird bugs with this 
+    # TODO this should be a Date/not care about time, required, not default to now, rename to "election day" or something
+    date = db.Column(db.DateTime, default=date.today())   
     office_name = db.Column(db.String(255))  # TODO eliminate in favor of a fk relationship with Office
 
     # office_id = db.Column(db.Integer, db.ForeignKey('office.id'))
+
+    candidates = db.relationship('Candidate', secondary='candidate_race', lazy='subquery',
+        backref=db.backref('races', lazy=True))
 
 
 class Office(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for
-from app import app 
+
+from app import app
+from app.models import Candidate, Race
 
 @app.route('/')
 def hello_world():
-    return render_template('base.html', content='page content here', title='Example Page')
+    candidates = Candidate.query.all()
+    for c in candidates: 
+        c.race_names = [f'{r.office_name} on {r.date.strftime("%B %e, %Y")}' for r in c.races]
+    return render_template(
+        'base.html', 
+        content='page content here', 
+        title='Example Page', 
+        candidates=candidates
+    )
